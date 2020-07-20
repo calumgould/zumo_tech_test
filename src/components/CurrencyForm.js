835ae247bac5e9ebdd5c98220a1f05017e8f675b
calumgould <calumgould26@gmail.com>
2020-07-20 21:25:58 +0100

    flag image displaying based on courntry largest population for selected currency symbol

--- src/components/CurrencyForm.js
+++ src/components/CurrencyForm.js
@@ -1,10 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import '../styles/CurrencyForm.css';
 
 const CurrencyForm = ({currencySymbols, setResults}) => {
 
     const [fetchError, setFetchError] = useState(false);
+    const [flagURL, setFlagURL] = useState('');
+
+    useEffect(() => {
+        
+    }, [])
+
+    const findFlag = async (event) => {
+        fetch(`https://restcountries.eu/rest/v2/currency/${event.target.value}`)
+        .then(res => res.json())
+        .then(data => {
+            let largestCountry = data.reduce((prev, curr) => prev.population > curr.population ? prev : curr);
+            setFlagURL(largestCountry.flag);
+        })
+        .catch(() => setFetchError(true));
+    }
 
     const currencyOptions = currencySymbols.map((currency, index) => {
         return <option key={index} value={currency}>{currency}</option>
@@ -29,7 +44,7 @@ const CurrencyForm = ({currencySymbols, setResults}) => {
         <div className='form-wrapper'>
             <form className='currency-form' onSubmit={handleSubmit}>
                 <label htmlFor='currencySymbol'>My Preferred Currency: </label>
-                <select name='currencySymbol' defaultValue='' required >
+                <select name='currencySymbol' defaultValue='' onChange={findFlag} required >
                     <option value='' disabled>Select currency</option>
                     {currencyOptions}
                 </select>
@@ -37,7 +52,8 @@ const CurrencyForm = ({currencySymbols, setResults}) => {
                 <input type='number' step='0.01' name='currencyAmount' placeholder='Enter amount...' required />
                 <button className='button' type='submit'>Convert</button>
             </form>
-            {fetchError && alert('Failed to fetch conversion data, please refresh the page and try again.')}
+            {fetchError && alert('Failed to fetch data, please refresh the page and try again.')}
+            <img src={flagURL} alt='selected country flag' />
         </div>
      );
 }
