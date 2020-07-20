ff1267dee0f97518754809cb1c3e41f9b24e8a7e
calumgould <calumgould26@gmail.com>
2020-07-20 19:33:29 +0100

    api data fetched based on inputted currency symbol

--- src/components/CurrencyInput.js
+++ src/components/CurrencyInput.js
@@ -1,22 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const CurrencyInput = ({currencySymbols}) => {
+
+    const [results, setResults] = useState({})
+    const [fetchError, setFetchError] = useState(false);
+
+    useEffect(() => {
+        console.log('results', results);
+    }, [results])
 
     const currencyOptions = currencySymbols.map((currency, index) => {
         return <option key={index} value={currency}>{currency}</option>
     })
 
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        let preferredCurrency = event.target.currencySymbol.value
+        fetch(`https://api.exchangeratesapi.io/latest?base=${preferredCurrency}`)
+        .then(res => res.json())
+        .then(data => setResults(data))
+        .catch(() => setFetchError(true));
+    }
+
     return ( 
         <>
-            <form>
-                <label for='currencySymbols'>My Currency: </label>
-                <select name='currencySymbols' defaultValue='' required >
+            <form onSubmit={handleSubmit}>
+                <label htmlFor='currencySymbol'>My Currency: </label>
+                <select name='currencySymbol' defaultValue='' required >
                     <option value='' disabled>Select preferred currency</option>
                     {currencyOptions}
                 </select>
-                <label for='currencyAmount'>Amount to convert: </label>
+                <label htmlFor='currencyAmount'>Amount to convert: </label>
                 <input type='number' step='0.01' name='currencyAmount' placeholder='Enter amount...' required />
+                <button type='submit'>Convert</button>
             </form>
+            {fetchError && alert('Failed to fetch conversion data, please refresh the page and try again.')}
         </>
      );
 }
